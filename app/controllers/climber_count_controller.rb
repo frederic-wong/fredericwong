a26c331eaec3db9b77c930d6178fa773cd0ea046
Frederic Wong <frederic.wong255@gmail.com>
2021-05-15 13:53:23 +0100

    add arrows to counter
    Signed-off-by: Frederic Wong <frederic.wong255@gmail.com>

--- app/controllers/climber_count_controller.rb
+++ app/controllers/climber_count_controller.rb
@@ -5,6 +5,7 @@ class ClimberCountController < ApplicationController
     @graph_data = [{ name: 'Climbers', data: ClimberCount.where(created_at: 7.days.ago..Date.today).pluck(:created_at, :climber) }]
     @current_climber_count = ClimberCount.last
     @color = define_color(@current_climber_count.climber)
+    @arrow = define_arrow
   end
 
   def define_color(count)
@@ -14,6 +15,15 @@ class ClimberCountController < ApplicationController
       'orange'
     elsif count >= 40
       'red'
+    end
+  end
+
+  def define_arrow
+    if ClimberCount.last.climber > ClimberCount.second_to_last.climber
+      'down'
+    elsif
+      ClimberCount.last.climber < ClimberCount.second_to_last.climber
+      'up'
     end
   end
 end
--- app/controllers/the_gym_count_controller.rb
+++ app/controllers/the_gym_count_controller.rb
@@ -5,6 +5,7 @@ class TheGymCountController < ApplicationController
     @graph_data = [{ name: 'Gym goers', data: TheGymCount.where(created_at: 7.days.ago..Date.today).pluck(:created_at, :gymer) }]
     @current_gym_count = TheGymCount.last
     @color = define_color(@current_gym_count.gymer)
+    @arrow = define_arrow
   end
 
   def define_color(count)
@@ -14,6 +15,15 @@ class TheGymCountController < ApplicationController
       'orange'
     elsif count >= 50
       'red'
+    end
+  end
+
+  def define_arrow
+    if ClimberCount.last.climber > ClimberCount.second_to_last.climber
+      'down'
+    elsif
+      ClimberCount.last.climber < ClimberCount.second_to_last.climber
+      'up'
     end
   end
 end
